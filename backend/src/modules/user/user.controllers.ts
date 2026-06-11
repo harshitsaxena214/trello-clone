c93070d0a2e2dbdd7f3089d549b7a0b0aa775f1f
Harshit Saxena <harshit214saxena@gmail.com>
2026-06-11 08:56:37 +0530

    feat: simplify user profile retrieval; remove organizations data from response

--- backend/src/modules/user/user.controllers.ts
+++ backend/src/modules/user/user.controllers.ts
@@ -4,18 +4,7 @@ import { prisma } from "../../lib/db";
 export const getuserProfile = async (req: Request, res: Response) => {
   const userId = req.user.id;
   try {
-    const user = await prisma.user.findUnique({
-      where: { id: userId },
-      include: {
-        organizations: {
-          select: {
-            id: true,
-            name: true,
-            slug: true,
-          },
-        },
-      },
-    });
+    const user = await prisma.user.findUnique({ where: { id: userId } });
     if (!user) {
       return res
         .status(404)
@@ -28,7 +17,6 @@ export const getuserProfile = async (req: Request, res: Response) => {
         name: user.name,
         email: user.email,
         isAccountVerified: user.isAccountVerified,
-        organizations: user.organizations,
       },
     });
   } catch (error: any) {
